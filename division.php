f7781f9b7bd3066c5bf4f51c100b866fd7928dc5
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-11-05 18:40:29 +0000

    Create division.php

---
+++ division.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?="${_GET['divide_first']} divided by ${_GET['divide_second']} is:"?>
+<h4><?=$_GET['divide_first']/$_GET['divide_second']?></h4>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
