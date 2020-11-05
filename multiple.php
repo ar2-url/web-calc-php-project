dca91f7f058e23b30ae3aadf21e83ad5b12b9b55
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-11-05 18:41:40 +0000

    Create multiple.php

---
+++ multiple.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+<?= "${_GET['multi_first']} multiple by ${_GET['multi_second']} is:" ?>
+<h4><?=$_GET['multi_first']*$_GET['multi_second']?></h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
