604a9a136a355984882efdd77683cf7110350579
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-11-05 18:38:04 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<h3>Addition</h3>
+<form method="get" action="addition.php">
+First Number: <input type="number" name="add_first"><br>
+Second Number: <input type="number" name="add_second"><br>
+<button type="submit">Add</button><br>
+</form>
+<h3>Subtract</h3>
+<form method="get" action="subtract.php">
+First Number: <input type="number" name="subt_first"><br>
+Second Number: <input type="number" name="subt_second"><br>
+<button type="submit">Subtract</button><br>
+</form>
+<h3>Multiple</h3>
+<form method="get" action="multiple.php">
+First Number: <input type="number" name="multi_first"><br>
+Second Number: <input type="number" name="multi_second"><br>
+<button type="submit">Multiple</button><br>
+</form>
+<h3>Division</h3>
+<form method="get" action="division.php">
+Numerator: <input type="number" name="divide_first"><br>
+Denominator: <input type="number" name="divide_second"><br>
+<button type="submit">Divide</button><br>
+</form>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
