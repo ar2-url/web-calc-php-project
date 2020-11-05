6f6c7a3afc7bceeef5887ccb101dbfe43de65322
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-11-05 18:43:09 +0000

    Create subtract.php

---
+++ subtract.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?="${_GET['subt_first']} minus ${_GET['subt_second']} is:"?>
+<h4><?=$_GET['subt_first'] - $_GET['subt_second']?></h4>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
